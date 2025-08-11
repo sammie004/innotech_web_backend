088a711f03db972dd9b1352e4835650f6624f0ac
sammie004 <123166502+sammie004@users.noreply.github.com>
2025-08-11 23:24:32 +0100

    new changes

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.get('/',(req,res)=>{
     res.send("Welcome to the Innotech API");
 })
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
